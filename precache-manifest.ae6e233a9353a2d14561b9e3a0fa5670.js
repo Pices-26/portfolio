9e6e87db233210dbc50d7d6abac3abc52eb5618d
Marcel <garfmaster3@gmail.com>
2020-05-26 21:42:43 +0100

    Updates

---
+++ precache-manifest.ae6e233a9353a2d14561b9e3a0fa5670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3350d60394d72309b99c87b66741ff20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65519ea8f97bc2e1b431",
+    "url": "/static/css/main.cf3f8aef.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65519ea8f97bc2e1b431",
+    "url": "/static/js/main.48d75b7b.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "f2df8a6568dda687f585a0e08b156d55",
+    "url": "/static/media/pices_logo.f2df8a65.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0147c63560ba7bfb1a788eb62fbb4df1.js"
+  "/precache-manifest.ae6e233a9353a2d14561b9e3a0fa5670.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
