b750557bcceaeb1bccaaa9ddf17e78dd3c8e90c1
Marcel <garfmaster3@gmail.com>
2020-05-26 18:25:22 +0100

    deploying react app portfolio template to be worked on

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import my_image from './img/pices_logo.jpg'
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Portfolio website</h1>
+      <img src={my_image} alt="profile_image"/>
+      <h2>Work in progress</h2>
+
     </div>
   );
 }
